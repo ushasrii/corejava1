6fbd95d10ad88eeab7f3cab042f79fe77fb76fdc
ushasrii <135214110+ushasrii@users.noreply.github.com>
2024-01-31 20:01:55 +0530

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,11 @@
+class Rectangle{
+	public static void main(String[] args){
+		double area,perimeter,length,breadth;
+		length=20;
+		breadth=10;
+		area=length*breadth;
+		perimeter=2*(length+breadth);
+		System.out.println("Area of a Rectangle is: "+area);
+		System.out.println("perimeter of a Rectangle is: "+perimeter);
+	}
+}
