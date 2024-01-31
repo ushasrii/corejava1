e580d519751f2024470d54c89efb688f3b20c46c
ushasrii <135214110+ushasrii@users.noreply.github.com>
2024-01-31 20:02:12 +0530

    Add files via upload

---
+++ Swapping.java
@@ -0,0 +1,10 @@
+class Swapping{
+	public static void main(String[] args){
+		int firstNum = 10,secondNum=5,temp;
+		temp=firstNum;
+		firstNum=secondNum;
+		secondNum=temp;
+		System.out.println("First Number is: "+firstNum);
+		System.out.println("Second Number is: "+secondNum);
+	}
+}
